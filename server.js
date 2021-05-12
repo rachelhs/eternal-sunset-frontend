46dd390918fd2cd4cdcb05a382c5ae1184f766ea
rachelhs <rachelsmith400@hotmail.co.uk>
2021-05-12 11:52:09 +0100

    tweak server for heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-// Load Node modules
-var express = require('express');
-// Initialise Express
-var app = express();
-// Render static files
-app.use(express.static('public'));
-// Port website will run on
-app.listen(5000);
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));

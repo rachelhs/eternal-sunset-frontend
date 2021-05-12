5b69aee800a8c1b635fe400fd9784f998374eff1
rachelhs <rachelsmith400@hotmail.co.uk>
2021-05-12 12:04:02 +0100

    install process pacakge

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
+const process = require('process');
+
 const api_key = process.env.api_key;
 const database_url = 'https://eternal-sunset-default-rtdb.europe-west1.firebasedatabase.app';
 const app_id = '1:721854210888:web:b6443f1e86c6a0095a097d';

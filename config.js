a5aa7c9a3307dddd304824b4920d85d16f4b3639
rachelhs <rachelsmith400@hotmail.co.uk>
2021-05-12 11:30:03 +0100

    switch to node

---
+++ config.js
@@ -0,0 +1,5 @@
+const api_key = process.env.api_key;
+const database_url = 'https://eternal-sunset-default-rtdb.europe-west1.firebasedatabase.app';
+const app_id = '1:721854210888:web:b6443f1e86c6a0095a097d';
+const messaging_sender_id = '721854210888';
+const measurement_id = 'G-CHZCZD99S4';
